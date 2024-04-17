6b626cd1b4e0b19b9cc96d1d8889643e4becdd65
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-04-17 21:26:09 +0200

    add session

--- tasks/views.py
+++ tasks/views.py
@@ -8,16 +8,11 @@ class NewTaskForm(forms.Form):
     task = forms.CharField()
 
 
-tasks = [
-    "Learn Python",
-    "Learn SQL",
-    "Learn Django"
-]
-
-
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 
@@ -26,7 +21,7 @@ def add_task(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
