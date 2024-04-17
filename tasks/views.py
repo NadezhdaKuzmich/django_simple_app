12f3ec902afd1bbe494a9d61c8acf092fae479c4
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-04-17 21:14:00 +0200

    edit app

--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,38 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField()
+
 
 tasks = [
     "Learn Python",
     "Learn SQL",
     "Learn Django"
 ]
+
+
 def index(request):
     return render(request, "tasks/index.html", {
         "tasks": tasks
     })
 
+
 def add_task(request):
-    return render(request, "tasks/add.html")
+    if request.method == 'POST':
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
